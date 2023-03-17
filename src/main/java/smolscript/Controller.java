709e978155500b267d9320649581c25c52f88b9c
Marius Erath <ge49lew@mytum.de>
2023-03-17 23:55:00 +0100

    Add Save button. Implement Save button.

--- src/main/java/smolscript/Controller.java
+++ src/main/java/smolscript/Controller.java
@@ -2,13 +2,35 @@ package smolscript;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Controller {
+
     @FXML
-    private Label welcomeText;
+    private TextArea scriptArea;
 
     @FXML
     protected void onRunButtonClick() {
+        onSaveButtonClick();
+        // TODO run script
+    }
 
+    @FXML
+    protected void onSaveButtonClick() {
+        String code = scriptArea.getText();
+
+        // Write code to file
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("script.kts", false));
+            writer.write(code);
+            writer.flush();
+        } catch (IOException e) {
+            System.err.println("Error when writing to file!");
+            //TODO add error popup
+        }
     }
 }
